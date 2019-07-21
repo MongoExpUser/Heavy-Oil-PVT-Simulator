12525088df662f6e4f705a93a2e3a93e9f53c73e
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2019-07-21 18:18:47 -0500

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,4 +1,4 @@
-/* @License Starts
+* @License Starts
  *
  * Copyright © 2015 - present. MongoExpUser
  *
@@ -24,128 +24,31 @@ public class Utility
   
  //contructor
  public void Utility(){}
-
- //A utility method for copying array to array
- double [] copy(double [] A){
-    int j = A.length;
-    double [] E = new double [j];
-    for(int i = 0; i < j; i++){
-      E[i] = A[i];
-    }
-    return E;
-  }
-
-  //A utility method for copying array of integer values to array of integer values
- int [] copyInt(int [] A){
-    int j = A.length;
-    int [] E = new int [j];
-    for(int i = 0; i < j; i++){
-      E[i] = A[i];
-    }
-    return E;
-  }
-
-  //A utility method for summing element of an array
- static double sumArray(double [] A){
-    int j = A.length;
-    double sum = 0;
-
-    for(int i = 0; i < j; i++){
-      sum = sum + A[i];
-    }
-
-    return sum;
-  }
-
-  //A utility for printing array elements to system prompt
-  static void printArrayToSystem(double [] A){
-    for(int i = 0; i < A.length; i++){
-          System.out.println( (i + 1)  +  ": "+ A[i] );
-      }
-  }
-
-  //A utility method for calculating complementary error function (erfc)
-  static double erfc(double x){
+ 
+ 
+  //a utility method for calculating complementary error function (erfc)
+  double erfc(double x){
     double  coefficients = (1 + 0.0705230784*x + 0.0422820123*x*x + 0.0092705272*x*x*x + 0.0001520143*x*x*x*x + 0.0002765672*x*x*x*x*x + 0.0000430638*x*x*x*x*x*x);
     double value = Math.pow(coefficients, -16);
     return value; //Source: Butler, R.M. (1997). pgs. 35 & 36. error in computation  <= Math.abs(3E-7)
   }
 
-  //A utility method for calculating error function (erf)
-  static double erf(double x){
+  //a utility method for calculating error function (erf)
+  double erf(double x){
     double  coefficients = (1 + 0.0705230784*x + 0.0422820123*x*x + 0.0092705272*x*x*x + 0.0001520143*x*x*x*x + 0.0002765672*x*x*x*x*x + 0.0000430638*x*x*x*x*x*x);
     double value = ( 1 - (Math.pow(coefficients, -16)) );
     return value; //Source: Butler, R.M. (1997). pgs 35 & 36. error in computation  <= Math.abs(3E-7)
   }
-
-  //A utility method for finding the maximum value from an array of values
-  static double maximumOfArrayValues(double [] A){
-     double staticMaximumValue = 0;
-     double dynamicMaximumValue = 0;
-     for(int i = 0; i < (A.length - 1); i++){
-       if(i == 0){
-         if(A[i] >= A[i+1]){
-           dynamicMaximumValue = A[i];
-           staticMaximumValue = dynamicMaximumValue;
-         }
-         else{
-           dynamicMaximumValue = A[i+1];
-           staticMaximumValue = dynamicMaximumValue;
-         }
-       }
-       else if(i > 0){
-         if(A[i] >= A[i+1]){
-           dynamicMaximumValue = A[i];
-           staticMaximumValue = Math.max(staticMaximumValue, dynamicMaximumValue);
-         }
-         else{
-           dynamicMaximumValue = A[i+1];
-           staticMaximumValue = Math.max(staticMaximumValue, dynamicMaximumValue);
-         }
-       }
-     }
-     double LastMaximumValue = staticMaximumValue;
-     return LastMaximumValue;
-   }
-
-  //A utility method for finding the minimum value from an array of values
-  static double minimumOfArrayValues(double [] A){
-     double staticMinimumValue = 0;
-     double dynamicMinimumValue = 0;
-     for(int i = 0; i < (A.length - 1); i++){
-       if(i == 0){
-         if(A[i] <= A[i+1]){
-           dynamicMinimumValue = A[i];
-           staticMinimumValue = dynamicMinimumValue;
-         }
-         else{
-           dynamicMinimumValue = A[i+1];
-           staticMinimumValue = dynamicMinimumValue;
-         }
-       }
-       else if(i > 0){
-         if(A[i] <= A[i+1]){
-           dynamicMinimumValue = A[i];
-           staticMinimumValue = Math.min(staticMinimumValue, dynamicMinimumValue);
-         }
-         else{
-           dynamicMinimumValue = A[i+1];
-           staticMinimumValue = Math.min(staticMinimumValue, dynamicMinimumValue);
-         }
-       }
-     }
-     double LastMinimumValue = staticMinimumValue;
-     return LastMinimumValue;
-   }
-
-   //A utility method for finding the maximum (largest) of the 3 roots of an EOS = vapour z-factor
-   static double vapourCompressibilty(double [] A){
+  
+  
+  //a utility method for finding the maximum (largest) of the 3 roots of an EOS = vapour z-factor
+  double vapourCompressibilty(double [] A){
      double Value = maximumOfArrayValues(A);
      return Value;
    }
 
-   //A utility method for finding the minimum (smallest) positive of the 3 roots of an EOS = liquid z-factor
-   static double liquidCompressibilty(double [] A){
+  //a utility method for finding the minimum (smallest) positive of the 3 roots of an EOS = liquid z-factor
+  double liquidCompressibilty(double [] A){
      double [] B = new double[A.length];
 
      for(int i = 0; i < A.length; i++){
@@ -159,13 +62,13 @@ public class Utility
      return minimumOfArrayValues(B);
    }
 
-  //Natural log
-  static double nl(double value){
+  //a utility method  for calculating natural log
+  double nl(double value){
     return Math.log(value)/Math.log(2.718281828459045);
   }
 
-  //A method for Newton-Jacobi solution method
-  static double [] newtonJacobi(double [] A, double [] B){
+  //a method for Newton-Jacobi solution method
+  double [] newtonJacobi(double [] A, double [] B){
    int j = A.length;
    double [] E = new double[j];
    for(int i = 0; i < j; i++){
@@ -174,8 +77,8 @@ public class Utility
    return E;
  }
 
-  //Thomas algorithm method
-  static double [] thomasAlgorithmSolution (double upperElements [], double diagonalElements [], double lowerElements [], double rhsElements []){
+  //implementation of thomas algorithm method for solving tri-diagonal matrix
+  double [] thomasAlgorithmSolution (double upperElements [], double diagonalElements [], double lowerElements [], double rhsElements []){
 
     int matrixSize = diagonalElements.length; //For an m x n tri-diagonal matrix, number of rows (m) = number of cols n = (matrix size)
 
@@ -239,4 +142,103 @@ public class Utility
    return E;
  }
 
+ //a utility method for copying array to array
+ double [] copy(double [] A){
+    int j = A.length;
+    double [] E = new double [j];
+    for(int i = 0; i < j; i++){
+      E[i] = A[i];
+    }
+    return E;
+  }
+
+  //a utility method for copying array of integer values to array of integer values
+ int [] copyInt(int [] A){
+    int j = A.length;
+    int [] E = new int [j];
+    for(int i = 0; i < j; i++){
+      E[i] = A[i];
+    }
+    return E;
+  }
+
+  //a utility method for summing element of an array
+ double sumArray(double [] A){
+    int j = A.length;
+    double sum = 0;
+
+    for(int i = 0; i < j; i++){
+      sum = sum + A[i];
+    }
+
+    return sum;
+  }
+
+  //a utility for printing array elements to system prompt
+ void printArrayToSystem(double [] A){
+    for(int i = 0; i < A.length; i++){
+          System.out.println( (i + 1)  +  ": "+ A[i] );
+      }
+  }
+
+ 
+  //a utility method for finding the maximum value from an array of values
+  double maximumOfArrayValues(double [] A){
+     double staticMaximumValue = 0;
+     double dynamicMaximumValue = 0;
+     for(int i = 0; i < (A.length - 1); i++){
+       if(i == 0){
+         if(A[i] >= A[i+1]){
+           dynamicMaximumValue = A[i];
+           staticMaximumValue = dynamicMaximumValue;
+         }
+         else{
+           dynamicMaximumValue = A[i+1];
+           staticMaximumValue = dynamicMaximumValue;
+         }
+       }
+       else if(i > 0){
+         if(A[i] >= A[i+1]){
+           dynamicMaximumValue = A[i];
+           staticMaximumValue = Math.max(staticMaximumValue, dynamicMaximumValue);
+         }
+         else{
+           dynamicMaximumValue = A[i+1];
+           staticMaximumValue = Math.max(staticMaximumValue, dynamicMaximumValue);
+         }
+       }
+     }
+     double LastMaximumValue = staticMaximumValue;
+     return LastMaximumValue;
+   }
+
+  //a utility method for finding the minimum value from an array of values
+  double minimumOfArrayValues(double [] A){
+     double staticMinimumValue = 0;
+     double dynamicMinimumValue = 0;
+     for(int i = 0; i < (A.length - 1); i++){
+       if(i == 0){
+         if(A[i] <= A[i+1]){
+           dynamicMinimumValue = A[i];
+           staticMinimumValue = dynamicMinimumValue;
+         }
+         else{
+           dynamicMinimumValue = A[i+1];
+           staticMinimumValue = dynamicMinimumValue;
+         }
+       }
+       else if(i > 0){
+         if(A[i] <= A[i+1]){
+           dynamicMinimumValue = A[i];
+           staticMinimumValue = Math.min(staticMinimumValue, dynamicMinimumValue);
+         }
+         else{
+           dynamicMinimumValue = A[i+1];
+           staticMinimumValue = Math.min(staticMinimumValue, dynamicMinimumValue);
+         }
+       }
+     }
+     double LastMinimumValue = staticMinimumValue;
+     return LastMinimumValue;
+   }
 }
